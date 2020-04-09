40b6215c1aef819750ec1a61088942f2c4f43a5a
Songbai Yan <colorguitar@hotmail.com>
2020-04-09 11:03:32 +0800

    add setup method

--- src/test/java/com/oocl/GuessNumberGameTest.java
+++ src/test/java/com/oocl/GuessNumberGameTest.java
@@ -1,13 +1,21 @@
 package com.oocl;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GuessNumberGameTest {
+    private GuessNumberGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        // before method
+        String answer = "1234";
+        game = new GuessNumberGame(answer);
+    }
+
     @Test
     public void should_return_4A0B_when_answer_is_1234_given_input_is_1234() {
-        String answer = "1234";
-        GuessNumberGame game = new GuessNumberGame(answer);
         String inputNumbers = "1234";
         String result = game.guess(inputNumbers);
 
@@ -16,8 +24,6 @@ public class GuessNumberGameTest {
 
     @Test
     public void should_return_2A2B_when_answer_is_1234_given_input_is_1243() {
-        String answer = "1234";
-        GuessNumberGame game = new GuessNumberGame(answer);
         String inputNumbers = "1243";
         String result = game.guess(inputNumbers);
 
